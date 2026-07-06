0565b6802153e69cb224bf4a0b248d0a8151ea59
leomazet0809-a11y <leomazet0809@gmail.com>
2026-07-06 07:00:25 +0200

    Update curiosity-logo.js

--- curiosity-logo.js
+++ curiosity-logo.js
@@ -6,8 +6,8 @@
    Fond transparent. Repos en boucle, s'anime au survol, "parle" au clic. */
 (function () {
   const PARAMS = {
-    nb: 1500, rayonK: 0.28, dispersion: 0.75, derive: 1.55, aplat: 1,
-    taille: 1, scint: 0.35, noyau: 0.8, vitesse: 0.75, intensite: 1.3
+    nb: 1000, rayonK: 0.28, dispersion: 0.75, derive: 1.55, aplat: 1,
+    taille: 1.8, scint: 0.35, noyau: 0.8, vitesse: 0.75, intensite: 1.3
   };
 
   function hex(h) {
